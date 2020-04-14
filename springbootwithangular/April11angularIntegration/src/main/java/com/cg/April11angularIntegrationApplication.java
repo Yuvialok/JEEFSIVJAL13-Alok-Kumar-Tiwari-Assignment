5a4490c4f3da00853d3d8e7c641c26fde525f266
Alok Kumar Tiwari <38471877+Yuvialok@users.noreply.github.com>
2020-04-14 16:57:18 +0530

    Add files via upload

---
+++ springbootwithangular/April11angularIntegration/src/main/java/com/cg/April11angularIntegrationApplication.java
@@ -0,0 +1,63 @@
+package com.cg;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cg.dao.StudentDaoI;
+import com.cg.entity.Student;
+
+@SpringBootApplication
+public class April11angularIntegrationApplication implements CommandLineRunner{
+
+	@Autowired
+	StudentDaoI studao;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(April11angularIntegrationApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	/*	 Student s1 = new Student(5, "rohit", "cs"); 
+		 Student s2 = new Student(6,"rahul","mech"); 
+		 Student s3 = new Student(7, "santhosh", "cs");
+		 Student s4 = new Student(8,"mahesh","civil");
+		 
+		 studao.create(s1);
+		 studao.create(s2);
+		 studao.create(s3);
+		 studao.create(s4);
+		 
+		 System.out.println("record inserted");         
+		 
+		 */
+		
+       
+		
+		 List<Student> studentlist =studao.reterive();
+		 
+		 for(Student stu:studentlist) 
+		 { 
+			 System.out.println(stu.getId() +" "+stu.getName() +" "+ stu.getDept() ); 
+	     }
+		 
+		
+		/*
+		 * Student stu=studao.findById(2); System.out.println(stu.getId() +" "+
+		 * stu.getName() + stu.getDept());
+		 */
+		
+		//studao.delete(3);
+		
+		//studao.update(4);
+		
+	}
+
+	
+
+}
